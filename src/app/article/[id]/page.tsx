265d72cce3f846ef99031941d3bf567beda015ca
불 드럼통119 <drumtong119@gmail.com>
2026-04-20 15:20:03 +0900

    fix: restore missing Link import

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { ArticleCard } from "../../components/ArticleCard";
